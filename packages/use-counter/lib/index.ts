f308bd308bad04e4b996bfc0226b0b8043091a61
Ross Bratton <bratton.ross@gmail.com>
2020-01-19 22:43:38 +0000

    feat(use-counter): implement useCounter

--- packages/use-counter/lib/index.ts
+++ packages/use-counter/lib/index.ts
@@ -1,1 +1,23 @@
-console.log('testing counter')
+import { ref } from 'vue'
+
+export default function useCounter(initialValue = 0) {
+  const count = ref(initialValue)
+
+  function increment(step = 1) {
+    count.value += step
+  }
+
+  function decrement(step = 1) {
+    count.value -= step
+  }
+
+  function set(value: number) {
+    count.value = value
+  }
+
+  function reset() {
+    count.value = initialValue
+  }
+
+  return { count, increment, decrement, set, reset }
+}
